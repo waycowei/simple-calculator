64218491c84e83ff3ecd0d8cc17b8d9cc7be8c18
wkcole <wkcole@foxmail.com>
2015-07-20 22:56:04 +0800

    0-9已ok

--- javascript/main.js
+++ javascript/main.js
@@ -5,85 +5,131 @@
 var isdeFor = 0;                    //0：整数 1：小数
 var isdeLat = 0;
 var sign = 0;                       //0：无操作 1：加 2：减 3：乘 4：除
+var timesLow = 1;                   //被操作数小数倍数
+var timesHig = 1;                   //操作数小数倍数
 
 var temp = 0;                       //记录上一轮结果
-var console = "";                   //显示当前公式
+var console = "0";                   //显示当前公式
 var oleConsole = "";                //显示上次公式结果
-//var former = 0;                     //被操作数
-//var later = 1;                      //操作数
-var timesLow = 0.1;                 //小数倍数
 var inteFor = 0;                    //被操作数整数部分
 var deciFor = 0;                    //被操作数小数部分
 var inteLat = 0;                    //操作数整数部分
 var deciLat = 0;                    //操作数小数部分
 
+//TODO:写BACK
+//TODO:写加减乘除
 
+//显示结果
+function show(s){
+    $(s).text("");
+    $(s).append(console);
+}
 
-//TODO:点击.重置decimal
+function add(s){
+    if(isdeLat){
+        //操作数的小数部分
+        deciLat = deciLat+n*(Math.pow(0.1,timesHig).toFixed(timesHig));
+        timesHig += 1;
+        console = (inteFor+deciFor).toString()+s+(inteLat+deciLat).toString();
+    }else{
+        //操作数的整数部分
+        inteLat = inteLat*10+n;
+        console = (inteFor+deciFor).toString()+s+inteLat.toString();
+    }
+}
 
-$("#5_2").click(function(){
-
+/**
+ *
+ * @param n
+ */
+function numKey(n){
     switch (sign){
         case 0:
-            if(!isdeFor){
-                inteFor = inteFor*10;
+            if(isdeFor){
+                //被操作数的小数部分
+                deciFor = deciFor+n*(Math.pow(0.1,timesLow).toFixed(timesLow));
+                console = (inteFor+deciFor).toFixed(timesLow).toString();
+                timesLow += 1;
+            }else{
+                //被操作数的整数部分
+                inteFor = inteFor*10+n;
                 console = inteFor.toString();
-            }else{
-                deciFor = deciFor*timesLow;
-                timesLow = timesLow*0.1;
-                console = (inteFor+deciFor).toString();
             }
+            break;
         case 1:
-            if(!isdeLat){
-                inteLat = inteLat*10;
-                console = (inteFor+deciFor).concat(" + ".concat(inteLat.toString()));
-            }else{
-                deciLat = deciLat*timesLow;
-                timesLow = timesLow*0.1;
-                console = (inteFor+deciFor).concat(" + ".concat((inteLat+inteLat).toString()));
-            }
+            add("+");
+            break;
         case 2:
-            if(!isdeLat){
-                inteLat = inteLat*10;
-                console = (inteFor+deciFor).concat(" - ".concat(inteLat.toString()));
-            }else{
-                deciLat = deciLat*timesLow;
-                timesLow = timesLow*0.1;
-                console = (inteFor+deciFor).concat(" - ".concat((inteLat+inteLat).toString()));
-            }
+            add("-");
+            break;
         case 3:
-            if(!isdeLat){
-                inteLat = inteLat*10;
-                console = (inteFor+deciFor).concat(" * ".concat(inteLat.toString()));
-            }else{
-                deciLat = deciLat*timesLow;
-                timesLow = timesLow*0.1;
-                console = (inteFor+deciFor).concat(" * ".concat((inteLat+inteLat).toString()));
-            }
+            add("*");
+            break;
         case 4:
-            if(!isdeLat){
-                inteLat = inteLat*10;
-                console = (inteFor+deciFor).concat(" / ".concat(inteLat.toString()));
-            }else{
-                deciLat = deciLat*timesLow;
-                timesLow = timesLow*0.1;
-                console = (inteFor+deciFor).concat(" / ".concat((inteLat+inteLat).toString()));
-            }
+            add("/");
+            break;
         default :
             break;
     }
-    alert("asdsa");
+    show(".console");
+}
+
+
+
+//clear
+$("#1_1").click(function(){
+    console="";
     $(".console").text("");
     $(".console").append(console);
+    inteFor = 0;
+    deciFor = 0;
+    inteLat = 0;
+    deciLat = 0;
 });
 
-$("#5_3").click(function(){
-
-})
-
-$("#1_4").click(function(){
-    sign = 3;
-    console = "*";
+//back
+$("#1_2").click(function(){
+    if(console.length){
+        console = console.substring(0, console.length-1);
+    }
     $(".console").text("");
     $(".console").append(console);
 })
+
+//point
+$("#5_3").click(function(){
+    console.substring(0,console.length);
+    switch (sign){
+        case 0:
+            console = console+".";
+            isdeFor = 1;
+            break;
+        case 1,2,3,4:
+            console = console+".";
+            isdeLat = 1;
+            break;
+        default :
+            break;
+    }
+    show(".console");
+})
+
+$("#5_2").click(function(){numKey(0);});    //0
+$("#4_1").click(function(){numKey(1);});    //1
+$("#4_2").click(function(){numKey(2);});    //2
+$("#4_3").click(function(){numKey(3);});    //3
+$("#3_1").click(function(){numKey(4);});    //4
+$("#3_2").click(function(){numKey(5);});    //5
+$("#3_3").click(function(){numKey(6);});    //6
+$("#2_1").click(function(){numKey(7);});    //7
+$("#2_2").click(function(){numKey(8);});    //8
+$("#2_3").click(function(){numKey(9);});    //9
+
+//muti
+$("#1_4").click(function(){
+    sign = 3;
+    console = console+"*"
+    $(".console").text("");
+    $(".console").append(console);
+})
+
